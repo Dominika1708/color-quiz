d6a27c5fe64e154b9b29cfe35d9e8e615208b6ff
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-08-28 15:55:26 +0200

    header logo animation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,28 @@
-import './Header.css'
+import "./Header.css";
 
 const Header = () => (
-    <header className='header'>
-        <div className='header-logo'>
-            <img src='../../public/colors.png' alt='header logo' width={50} height={50}></img>
-            <p className='header-title'>COLOR QUIZ</p>
-        </div>
-    </header>
+  <header className="header">
+    <div className="header-logo">
+      <img
+        className="header-img"
+        src="../../public/colors.png"
+        alt="header logo"
+        width={50}
+        height={50}
+      ></img>
+      <p className="header-title">
+        <span>C</span>
+        <span>O</span>
+        <span>L</span>
+        <span>O</span>
+        <span>R</span>
+        <span> Q</span>
+        <span>U</span>
+        <span>I</span>
+        <span>Z</span> 
+      </p>
+    </div>
+  </header>
 );
 
-export default Header
+export default Header;
