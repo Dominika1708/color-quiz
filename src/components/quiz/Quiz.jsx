383189e36ab31c7b0f8cd35746992155d793660c
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-09-02 22:25:31 +0200

    quiz styles

--- src/components/quiz/Quiz.jsx
+++ src/components/quiz/Quiz.jsx
@@ -1,30 +1,38 @@
 import { useState } from "react";
+import './Quiz.css'
+
+const findCenterColor = (a, b) => {
+
+}
 
 const Quiz = () => {
   const [colorOne, setColorOne] = useState("#ffffff");
   const [colorTwo, setColorTwo] = useState("#ffffff");
 
+  // const combinedColors = findCenterColor(colorOne, colorTwo)
+
   return (
-    <>
-      <div>
-        <button
-          style={{ backgroundColor: colorOne }}
-          onClick={() =>
-            setColorOne("#" + Math.floor(Math.random() * 16777215).toString(16))
-          }
-        >
-          {colorOne}
-        </button>
-        <button
-          style={{ backgroundColor: colorTwo }}
-          onClick={() =>
-            setColorTwo("#" + Math.floor(Math.random() * 16777215).toString(16))
-          }
-        >
-          {colorTwo}
-        </button>
-      </div>
-    </>
+    <div className="quiz">
+      <button
+        className="quiz-button"
+        style={{ backgroundColor: colorOne }}
+        onClick={() =>
+          setColorOne("#" + Math.floor(Math.random() * 16777215).toString(16))
+        }
+      >
+        {colorOne}
+      </button>
+      <button
+        className="quiz-button"
+        style={{ backgroundColor: colorTwo }}
+        onClick={() =>
+          setColorTwo("#" + Math.floor(Math.random() * 16777215).toString(16))
+        }
+      >
+        {colorTwo}
+      </button>
+      {/* <p style={{ backgroundColor: combinedColors }}>{combinedColors}</p> */}
+    </div>
   );
 };
 
