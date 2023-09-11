dee39ceccf5e6278796b2971a93f5c889e522191
Dominika Sosnowska <domi.sosnowska@o2.pl>
2023-09-11 21:46:51 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,10 @@ import Header from './components/header/Header'
 import Quiz from './components/quiz/Quiz'
 
 function App() {
-  const [color, setColor] = useState("#ffffff")
 
   return (
     <>
       <Header />
-      <img src='../public/color-logo.png' alt='logo' width={200}></img>
-      <h1>Color quiz</h1>
       <Quiz/>
     </>
   )
